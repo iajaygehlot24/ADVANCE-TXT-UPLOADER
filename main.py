7d03e1716a0fef906c9ed9200ce99dc0deebbfc3
iajaygehlot24 <starajgehlot@gmail.com>
2025-05-28 22:12:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -562,12 +562,14 @@ async def upload(bot: Client, m: Message):
     await input6.delete(True)
     await editable.delete()
 
-    #thumb = input6.text
-    #if thumb.startswith("http://") or thumb.startswith("https://"):
-        #getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
-        #thumb = "thumb.jpg"
-    else:
-        thumb == "no"
+    thumb = input6.text
+if thumb.startswith("http://") or thumb.startswith("https://"):
+    import subprocess
+    subprocess.run(["wget", thumb, "-O", "thumb.jpg"])  # Download the thumbnail
+    thumb = "thumb.jpg"
+else:
+    thumb = "no"
+
     failed_count =0
     if len(links) == 1:
         count = 1
